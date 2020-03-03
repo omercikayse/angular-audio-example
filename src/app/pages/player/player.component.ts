fa32839c79095f9285326635d209882d81151712
Omercik, Ayse <ayse.omercik@accenture.com>
2020-03-03 16:18:26 +0300

    added audio and cloud services

--- src/app/pages/player/player.component.ts
+++ src/app/pages/player/player.component.ts
@@ -1,39 +1,80 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
+import { StreamState } from 'src/app/interfaces/stream-state';
+import { AudioService } from 'src/app/services/audio.service';
+import { CloudService } from 'src/app/services/cloud.service';
 
 @Component({
-  selector: "app-player",
-  templateUrl: "./player.component.html",
-  styleUrls: ["./player.component.scss"]
+  selector: 'app-player',
+  templateUrl: './player.component.html',
+  styleUrls: ['./player.component.scss']
 })
 export class PlayerComponent implements OnInit {
-  public files: Array<any> = [
-    { name: "First song", artist: "Inder" },
-    { name: "Second Song", artist: "You" }
-  ];
-  public state;
+  public files: Array<any> = [];
+  public state: StreamState;
   public currentFile: any = {};
 
-  constructor() {}
+  constructor(
+    private audioService: AudioService,
+    private cloudService: CloudService
+  ) {
+    // get media files
+    this.cloudService.getFiles().subscribe(files => {
+      this.files = files;
+    });
+
+    // listen to stream state
+    this.audioService.getState().subscribe(state => {
+      this.state = state;
+    });
+  }
 
   ngOnInit(): void {}
 
-  public isFirstPlaying() {
-    return false;
+  playStream(url) {
+    this.audioService.playStream(url).subscribe(events => {
+      // listening for fun here
+    });
   }
 
-  public isLastPlaying() {
-    return true;
+  openFile(file, index) {
+    this.currentFile = { index, file };
+    this.audioService.stop();
+    this.playStream(file.url);
   }
 
-  public openFile(file, i) {}
+  pause() {
+    this.audioService.pause();
+  }
 
-  public onSliderChangeEnd(event) {}
+  play() {
+    this.audioService.play();
+  }
 
-  public next() {}
+  stop() {
+    this.audioService.stop();
+  }
 
-  public play() {}
+  next() {
+    const index = this.currentFile.index + 1;
+    const file = this.files[index];
+    this.openFile(file, index);
+  }
 
-  public previous() {}
+  previous() {
+    const index = this.currentFile.index - 1;
+    const file = this.files[index];
+    this.openFile(file, index);
+  }
 
-  public pause() {}
+  isFirstPlaying() {
+    return this.currentFile.index === 0;
+  }
+
+  isLastPlaying() {
+    return this.currentFile.index === this.files.length - 1;
+  }
+
+  onSliderChangeEnd(change) {
+    this.audioService.seekTo(change.value);
+  }
 }
